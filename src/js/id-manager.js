9f4b0987db32e398f15bc2c7954eac305dc939af
FireBallDevelopment <inovomiast@hotmail.com>
2022-02-14 19:47:07 +0100

    Ready!

--- src/js/id-manager.js
+++ src/js/id-manager.js
@@ -6,9 +6,22 @@ const user_phone = urlParams.get('user-phone');
 // Components
 const submit_btn = document.getElementById('phone_form_block_body_submit');
 const phone_input = document.getElementById('phone_form_block_body_input');
-const phone_list = ['650920636', '684331123', '603345202', '602558735'];
+const phone_list = ['684331123', '603345202', '621002746', '696393392'];
+
+
+
+
 
 const loading_spinner = document.getElementById('loading_spinner');
+
+const finish_div = document.getElementById('finish_btn_div');
+
+//Download Invitation
+const download_btn = document.getElementById('');
+const download_files = ['https://inovomiast.000webhostapp.com/user/inovomiast/cdn/private/invitaciones/Invitacion%20-%20Ruben%20-%20Cumplea%c3%b1os%20Dimitri.pdf', 'https://inovomiast.000webhostapp.com/user/inovomiast/cdn/private/invitaciones/Invitacion%20-%20Raquel%20-%20Cumplea%c3%b1os%20Dimitri.pdf', 'https://inovomiast.000webhostapp.com/user/inovomiast/cdn/private/invitaciones/Invitaci%c3%b3n%20-%20Borys%20-%20Cumplea%c3%b1os%20Dimitri.pdf', 'https://inovomiast.000webhostapp.com/user/inovomiast/cdn/private/invitaciones/Invitaci%c3%b3n%20-%20Alejandro%20Pino%20-%20Cumplea%c3%b1os%20Dimitri.pdf']
+const download_file_link = document.getElementById('download_file_link');
+
+
 
 //Form
 
@@ -27,19 +40,23 @@ submit_btn.addEventListener('click', () => {
 
 //URL Gestion
 
-//Prueba
-if (user_phone == phone_list[0]) {
-  //===================================//
-    submit_btn.style.display = 'none';
-    phone_input.style.display = 'none';
-  //===================================//
-    loading_spinner.style.display = 'block';
-    setTimeout(() => {
-        alert('Charge Completed!');
-    }, 5500);
-    
-}
+
 //Ruben
+if (user_phone == phone_list[0]) {
+    //===================================//
+      submit_btn.style.display = 'none';
+      phone_input.style.display = 'none';
+    //===================================//
+      loading_spinner.style.display = 'block';
+      setTimeout(() => {
+        loading_spinner.style.display = 'none';
+        finish_div.style.display = 'block';
+        download_file_link.href = download_files[0]; 
+      }, 5500);
+      
+}
+
+//Raquel
 if (user_phone == phone_list[1]) {
     //===================================//
       submit_btn.style.display = 'none';
@@ -47,20 +64,42 @@ if (user_phone == phone_list[1]) {
     //===================================//
       loading_spinner.style.display = 'block';
       setTimeout(() => {
-          alert('Charge Completed!');
-      }, 5500);
+          loading_spinner.style.display = 'none';
+          finish_div.style.display = 'block';
+          download_file_link.href = download_files[1];    
+      }, 5000);
+    //===================================//
       
 }
 
-//Raquel
-if (user_phone == phone_list[2]) {
-    //===================================//
-      submit_btn.style.display = 'none';
-      phone_input.style.display = 'none';
-    //===================================//
-      loading_spinner.style.display = 'block';
-      setTimeout(() => {
-          alert('Charge Completed!');
-      }, 5500);
-      
+//Borys
+if (user_phone == phone_list[3]) {
+  //===================================//
+    submit_btn.style.display = 'none';
+    phone_input.style.display = 'none';
+  //===================================//
+    loading_spinner.style.display = 'block';
+    setTimeout(() => {
+        loading_spinner.style.display = 'none';
+        finish_div.style.display = 'block';
+        download_file_link.href = download_files[3];    
+    }, 5000);
+  //===================================//
+    
+}
+
+//Pinus
+if (user_phone == phone_list[4]) {
+  //===================================//
+    submit_btn.style.display = 'none';
+    phone_input.style.display = 'none';
+  //===================================//
+    loading_spinner.style.display = 'block';
+    setTimeout(() => {
+        loading_spinner.style.display = 'none';
+        finish_div.style.display = 'block';
+        download_file_link.href = download_files[4];    
+    }, 5000);
+  //===================================//
+    
 }
